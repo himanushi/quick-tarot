3f3ab3bf11c71059d811ae87aef875cdf0df5e66
himanushi <himanushi@users.noreply.github.com>
2023-03-23 00:47:32 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,58 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+import * as PIXI from 'pixi.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = new PIXI.Application({ background: '#1099bb' });
+document.body.appendChild(app.view  as HTMLCanvasElement);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const basicText = new PIXI.Text('Basic text in pixi');
+basicText.x = 50;
+basicText.y = 100;
+
+app.stage.addChild(basicText);
+
+const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+});
+
+const richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
+richText.x = 50;
+richText.y = 220;
+
+app.stage.addChild(richText);
+
+const skewStyle = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    dropShadow: true,
+    dropShadowAlpha: 0.8,
+    dropShadowAngle: 2.1,
+    dropShadowBlur: 4,
+    dropShadowColor: '0x111111',
+    dropShadowDistance: 10,
+    fill: ['#ffffff'],
+    stroke: '#004620',
+    fontSize: 60,
+    fontWeight: 'lighter',
+    lineJoin: 'round',
+    strokeThickness: 12,
+});
+
+const skewText = new PIXI.Text('SKEW IS COOL', skewStyle);
+skewText.skew.set(0.65, -0.3);
+skewText.anchor.set(0.5, 0.5);
+skewText.x = 300;
+skewText.y = 480;
+
+app.stage.addChild(skewText);
